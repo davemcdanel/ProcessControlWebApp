9117759d969f7ec9db67e97a5e3c33599ff94ca6
davemcdanel <dave.mcdanel@gmail.com>
2021-11-18 20:15:20 -0600

    Added Chart.js

--- main.js
+++ main.js
@@ -225,6 +225,8 @@ function join() {
                   case 'derv':
                     set_derv.value = dataObject.payload;
                     break;
+                  case 'dataPoint':
+
                   default:
                     break;
                 }
@@ -335,6 +337,44 @@ function startSW (){
  }
 }
 
+$(document).ready(function(){
+
+const ctx = document.getElementById('myChart').getContext('2d');
+const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
 connectButton.addEventListener('click', join);
 set_setpoint.addEventListener('blur',send_set_setpoint);
 set_prop.addEventListener('blur',send_set_prop);
@@ -344,4 +384,6 @@ set_derv.addEventListener('blur',send_set_derv);
 startSW();
 initializePeerJS(); // Since all our callbacks are setup, start the process of obtaining an ID
 sendRequest();
-initializeGraph()
+initializeGraph();
+
+});
