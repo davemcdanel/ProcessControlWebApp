0de1a3e758607cc09650f5d53c3fffc70484d9eb
davemcdanel <dave.mcdanel@gmail.com>
2021-11-19 20:24:31 -0600

    typo

--- main.js
+++ main.js
@@ -345,52 +345,49 @@ function startSW (){
  }
 }
 
-$(document).ready(function(){
-  const ctx = document.getElementById('myChart').getContext('2d');
-  const myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-      labels: ['Temp', 'Setpoint', 'Internal', 'Output'],
-      datasets: [{
-        label: 'Cook Data',
-        data: 0,
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)'
-          //'rgba(153, 102, 255, 0.2)',
-          //'rgba(255, 159, 64, 0.2)'
-        ],
-        borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)'
-            //'rgba(153, 102, 255, 1)',
-            //'rgba(255, 159, 64, 1)'
-        ],
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: ['Temp', 'Setpoint', 'Internal', 'Output'],
+    datasets: [{
+      label: 'Cook Data',
+      data: 0,
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)'
+        //'rgba(153, 102, 255, 0.2)',
+        //'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)'
+          //'rgba(153, 102, 255, 1)',
+          //'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
       }
     }
-  });
-
-  connectButton.addEventListener('click', join);
-  set_setpoint.addEventListener('blur',send_set_setpoint);
-  set_prop.addEventListener('blur',send_set_prop);
-  set_inter.addEventListener('blur',send_set_inter);
-  set_derv.addEventListener('blur',send_set_derv);
-
-  startSW();
-  initializePeerJS(); // Since all our callbacks are setup, start the process of obtaining an ID
-  sendRequest();
-  initializeGraph();
-
+  }
 });
+
+connectButton.addEventListener('click', join);
+set_setpoint.addEventListener('blur',send_set_setpoint);
+set_prop.addEventListener('blur',send_set_prop);
+set_inter.addEventListener('blur',send_set_inter);
+set_derv.addEventListener('blur',send_set_derv);
+
+startSW();
+initializePeerJS(); // Since all our callbacks are setup, start the process of obtaining an ID
+sendRequest();
+initializeGraph();
