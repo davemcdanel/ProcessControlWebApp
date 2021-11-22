d420dcddd7d785dd0d2725f5d839dbcaba7b6d7c
davemcdanel <dave.mcdanel@gmail.com>
2021-11-21 20:24:10 -0600

    Turn off intersect.

--- main.js
+++ main.js
@@ -356,25 +356,29 @@ var myChart = new Chart(ctx, {
       data: 0,
       backgroundColor:'rgba(255, 99, 132, 0.2)',
       borderColor:'rgba(255, 99, 132, 1)',
-      borderWidth:1
+      borderWidth:1,
+      intersect: false
     },{
       label:'Setpoint',
       data:0,
       backgroundColor:'rgba(54, 162, 235, 0.2)',
       borderColor:'rgba(54, 162, 235, 1)',
-      borderWidth:1
+      borderWidth:1,
+      intersect: false
     },{
       label:'Internal',
       data:0,
       backgroundColor:'rgba(255, 206, 86, 0.2)',
       borderColor:'rgba(255, 206, 86, 1)',
-      borderWidth:1
+      borderWidth:1,
+      intersect: false
     },{
       label:'Output',
       data:0,
       backgroundColor:'rgba(75, 192, 192, 0.2)',
       borderColor:'rgba(75, 192, 192, 1)',
-      borderWidth:1
+      borderWidth:1,
+      intersect: false
     }]},
   options: {
     scales: {
