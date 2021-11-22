413fa088d4f1cf0f7fd4c9afeee94fd9ba3611f8
davemcdanel <dave.mcdanel@gmail.com>
2021-11-21 18:56:29 -0600

    Changed data format for chart.

--- main.js
+++ main.js
@@ -349,27 +349,30 @@ var myChart = new Chart(ctx, {
   type: 'line',
   data: {
     datasets: [{
-      label: ['Temp','Setpoint','Internal','Output'],
-      data: [0,0,0,0],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)'
-        //'rgba(153, 102, 255, 0.2)',
-        //'rgba(255, 159, 64, 0.2)'
-      ],
-      borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)'
-          //'rgba(153, 102, 255, 1)',
-          //'rgba(255, 159, 64, 1)'
-      ],
-      borderWidth: 1
-    }]
-  },
+      label:'Temp',
+      data: 0,
+      backgroundColor:'rgba(255, 99, 132, 0.2)',
+      borderColor:'rgba(255, 99, 132, 1)',
+      borderWidth:1
+    },{
+      label:'Setpoint',
+      data:0,
+      backgroundColor:'rgba(54, 162, 235, 0.2)',
+      borderColor:'rgba(54, 162, 235, 1)',
+      borderWidth:1
+    },{
+      label:'Internal',
+      data:0,
+      backgroundColor:'rgba(255, 206, 86, 0.2)',
+      borderColor:'rgba(255, 206, 86, 1)',
+      borderWidth:1
+    },{
+      label:'Output',
+      data:0,
+      backgroundColor:'rgba(75, 192, 192, 0.2)',
+      borderColor:'rgba(75, 192, 192, 1)',
+      borderWidth:1
+    }]},
   options: {
     scales: {
       y: {
