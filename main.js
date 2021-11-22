13d2dfc1a7a6b920382a475913955c34edd84960
davemcdanel <dave.mcdanel@gmail.com>
2021-11-21 19:51:01 -0600

    fix typo

--- main.js
+++ main.js
@@ -232,8 +232,8 @@ function join() {
                       myChart.data.labels.push(dataObject.payload['Time']);
                       myChart.data.datasets[0].data.push(dataObject.payload['Temp']);
                       myChart.data.datasets[1].data.push(dataObject.payload['Setpoint']);
-                      myChart.data.datasets[0].data.push(dataObject.payload['Internal']);
-                      myChart.data.datasets[1].data.push(dataObject.payload['Output']);
+                      myChart.data.datasets[2].data.push(dataObject.payload['Internal']);
+                      myChart.data.datasets[3].data.push(dataObject.payload['Output']);
                       //myChart.data.datasets.forEach((dataset) => [dataset.data.push(dataObject.payload['Temp']),
                       //                                            dataset.data.push(dataObject.payload['Setpoint']),
                       //                                            dataset.data.push(dataObject.payload['Internal']),
