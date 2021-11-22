167943bb4fd2c1c7510ff3bd138f6eab3e32a239
davemcdanel <dave.mcdanel@gmail.com>
2021-11-21 20:29:32 -0600

    Changed fill to false, added Interaction intersect false, radius 0.

--- main.js
+++ main.js
@@ -357,28 +357,44 @@ var myChart = new Chart(ctx, {
       backgroundColor:'rgba(255, 99, 132, 0.2)',
       borderColor:'rgba(255, 99, 132, 1)',
       borderWidth:1,
-      intersect: false
+      fill: false,
+      interaction: {
+        intersect: false
+        },
+      radius: 0
     },{
       label:'Setpoint',
       data:0,
       backgroundColor:'rgba(54, 162, 235, 0.2)',
       borderColor:'rgba(54, 162, 235, 1)',
       borderWidth:1,
-      intersect: false
+      fill: false,
+      interaction: {
+        intersect: false
+        },
+      radius: 0
     },{
       label:'Internal',
       data:0,
       backgroundColor:'rgba(255, 206, 86, 0.2)',
       borderColor:'rgba(255, 206, 86, 1)',
       borderWidth:1,
-      intersect: false
+      fill: false,
+      interaction: {
+        intersect: false
+      },
+      radius: 0
     },{
       label:'Output',
       data:0,
       backgroundColor:'rgba(75, 192, 192, 0.2)',
       borderColor:'rgba(75, 192, 192, 1)',
       borderWidth:1,
-      intersect: false
+      fill: false,
+      interaction: {
+        intersect: false
+      },
+      radius: 0
     }]},
   options: {
     scales: {
