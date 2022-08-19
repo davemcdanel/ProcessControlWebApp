eb00a97ab98d053a3e7dc26075167b344aeb654e
davemcdanel <dave.mcdanel@gmail.com>
2022-08-18 20:22:26 -0500

    updated path

--- main.js
+++ main.js
@@ -342,7 +342,7 @@ function send_set_derv() {
 function startSW (){
   if ('serviceWorker' in navigator){
     try {
-      navigator.serviceWorker.register('sw.js');
+      navigator.serviceWorker.register('./sw.js');
       console.log('SW registered');
     } catch (error) {
       console.log('SW reg failed');
