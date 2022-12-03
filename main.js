f82ae328afb192d1bd35f77ad568ea883380bae0
dave@kc0bpo.net <dave.mcdanel@gmail.com>
2022-12-02 18:41:17 -0600

    troubleshooting

--- main.js
+++ main.js
@@ -257,14 +257,14 @@ function join() {
                                 'Setpoint:' + dataObject.payload['Setpoint'] +
                                 'Internal:' + dataObject.payload['Internal'] +
                                 'Output:' + dataObject.payload['Output']);
-                    if((dataObject.payload['Time'] > 0) && (dataObject.payload['Time'])){
+                    //if((dataObject.payload['Time'] > 0) && (dataObject.payload['Time'])){
                       myChart.data.labels.push(dataObject.payload['Time']);
                       myChart.data.datasets[0].data.push(dataObject.payload['Temp']);
                       myChart.data.datasets[1].data.push(dataObject.payload['Setpoint']);
                       myChart.data.datasets[2].data.push(dataObject.payload['Internal']);
                       myChart.data.datasets[3].data.push(dataObject.payload['Output']);
                       myChart.update();
-                    }
+                    //}
                     
                   }
                   break;
